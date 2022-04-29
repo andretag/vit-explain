b2b4501c43edcec2c2569e85586cbec759009910
Andrea Tagliabue <andrea.tagliabue@me.com>
2022-04-29 10:00:23 -0400

    Package

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
+import setuptools
 
-from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(
-    name='vit_explain',
-    version='0.0.1',
-    description='VIT analysis',
-    author='TODO',
-    author_email='TODO',
-    python_requires=">=3.7.0",
-    url='https://github.com/TODO',
-    packages=find_packages(include=['vit_explain', 'vit_explain.*']),
-    install_requires=[],
-      include_package_data=True,
+setuptools.setup(
+    name="vit_explain",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
